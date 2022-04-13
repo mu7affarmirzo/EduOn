35a15f73f433cf3125074f00a38794485311496e
Rakhmatjon Khasanov <r.khasanov97@gmail.com>
2022-04-13 11:54:34 +0500

    Dockerize app

--- eduon_v1/settings.py
+++ eduon_v1/settings.py
@@ -13,6 +13,10 @@ import os
 from pathlib import Path
 from decouple import config
 
+
+def get_list(text):
+    return [item.strip() for item in text.split(",")]
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = get_list(os.environ.get("ALLOWED_HOSTS", "eduon.uz,localhost,127.0.0.1,0.0.0.0"))
 
 
 # Application definition
@@ -86,8 +90,12 @@ WSGI_APPLICATION = 'eduon_v1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
