e96e5b639c9faa3f30b51548709fb2e28e58a772
Rakhmatjon Khasanov <r.khasanov97@gmail.com>
2022-04-14 11:53:11 +0500

    fix setting DJANGO_SETTINGS_MODULE in settings.py

--- eduon_v1/celery.py
+++ eduon_v1/celery.py
@@ -1,10 +1,12 @@
+from __future__ import absolute_import, unicode_literals
+
 import os
 
-from tenant_schemas_celery.app import CeleryApp
+from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eduon_v1.settings")
 
-app = CeleryApp("eduon_v1")
+app = Celery("eduon_v1")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
