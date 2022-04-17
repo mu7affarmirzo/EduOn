53d7f80bfbd3bcb92f0bf5c9fabbfb516b5783b2
Rakhmatjon Khasanov <r.khasanov97@gmail.com>
2022-04-17 12:53:26 +0500

    Remove unnecessary code

--- api/v1/accounts/urls.py
+++ api/v1/accounts/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path, include
-from api.v1.accounts.views import hi, registration_view, account_list_view
+from django.urls import path
+from api.v1.accounts.views import registration_view, account_list_view
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('hi', hi, name='hi'),
     path('register', registration_view, name='registration'),
     path('list', account_list_view, name='list'),
 ]
