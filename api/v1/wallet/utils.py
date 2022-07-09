b28c44ade619eaecac1643c4456d22a4fb0a3d4e
mu7affarmirzo <cmuzaffarmirzo@gmail.com>
2022-07-09 15:54:21 +0500

    updated wallet and added some services

--- api/v1/wallet/utils.py
+++ api/v1/wallet/utils.py
@@ -5,7 +5,10 @@ from decouple import config
 
 # from eduon_v1 import settings
 from django.conf import settings
+from rest_framework import status
 from rest_framework.response import Response
+
+from wallet.models import TransferModel
 
 WALLET_URL = config('WALLET_URL')
 
@@ -65,3 +68,66 @@ def create_wallet_util(phone_number):
             return {'result': {'card_number': '0000', 'expire': '0000'}}
         return data.json()
 
+
+def withdraw_from_wallet_service(wallet, data):
+    payload = {
+        "id": "{{$randomUUID}}",
+        "method": "transfer.proceed",
+        "params": {
+            "number": f"{wallet.card_number}",
+            "expire": f"{wallet.expire}",
+            "receiver": f"{data['number']}",
+            "amount": f"{data['amount']}",
+        }
+    }
+    try:
+        resp_data = requests.post(url=WALLET_URL, json=payload, headers={"token": f"{settings.WALLET_TOKEN}"})
+    except:
+        return Response(status.HTTP_404_NOT_FOUND)
+
+    if resp_data.json()['status']:
+        TransferModel.objects.create(wallet=wallet, tr_id=resp_data.json()['result']['tr_id'])
+        return Response(data.json())
+    else:
+        token = login_to()
+        try:
+            resp_data = requests.post(url=WALLET_URL, json=payload, headers={"token": f"{token}"})
+        except:
+            return Response({'message': 'Service is not working.'})
+
+        try:
+            TransferModel.objects.create(wallet=wallet, tr_id=resp_data.json()['result']['tr_id'])
+        except:
+            resp_data.json()['transfer_status'] = "false"
+        return Response(resp_data.json())
+
+
+def transfer_service(wallet, data):
+    payload = {
+        "id": "{{$randomUUID}}",
+        "method": "transfer.proceed",
+        "params": {
+            "number": f"{data['number']}",
+            "expire": f"{data['expire']}",
+            "receiver": f"{wallet.card_number}",
+            "amount": f"{data['amount']}",
+        }
+    }
+    try:
+        resp_data = requests.post(url=WALLET_URL, json=payload, headers={"token": f"{settings.WALLET_TOKEN}"})
+        if resp_data.json()['status']:
+            return Response(resp_data.json())
+        else:
+            token = login_to()
+            try:
+                resp_data = requests.post(url=WALLET_URL, json=payload, headers={"token": f"{token}"})
+                print(payload)
+            except:
+                return Response({'message': 'Service is not working.'})
+            try:
+                TransferModel.objects.create(wallet=wallet, tr_id=resp_data.json()['result']['tr_id'])
+            except:
+                resp_data.json()['transfer_status'] = "false"
+        return Response(resp_data.json())
+    except:
+        return Response(status.HTTP_404_NOT_FOUND)
--- api/v1/wallet/views.py
+++ api/v1/wallet/views.py
@@ -1,5 +1,4 @@
 import os
-
 import requests
 
 from decouple import config
@@ -12,27 +11,22 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from api.v1.wallet.serializers import TransferSerializer, CardSerializer, WalletHistorySerializer, VoucherSerializer
-from api.v1.wallet.utils import login_to
-# from eduon_v1.settings import WALLET_TOKEN
+from api.v1.wallet.utils import login_to, withdraw_from_wallet_service, transfer_service
 from django.conf import settings
 from wallet.models import WalletModel, TransferModel, CardModel, VoucherModel
 
 
+
+EDUON_WALLET = config('EDUON_WALLET')
 WALLET_URL = config('WALLET_URL')
-# HEADER = {"token": f"{os.environ.get('WALLET_TOKEN')}"}
 HEADER = {"token": f"{settings.WALLET_TOKEN}"}
 
-PAYLOAD = {
-    "id": "{{$randomUUID}}",
-    "method": "",
-    "params": {}
-}
 
-
+@permission_classes([IsAuthenticated])
 @swagger_auto_schema(method="get", tags=["wallet"])
-@permission_classes((IsAuthenticated,))
 @api_view(['GET'])
 def info_wallet(request):
+
     try:
         wallet = WalletModel.objects.get(owner=request.user)
     except WalletModel.DoesNotExist:
@@ -62,6 +56,13 @@ def info_wallet(request):
         return Response(data.json())
 
 
+PAYLOAD = {
+    "id": "{{$randomUUID}}",
+    "method": "",
+    "params": {}
+}
+
+
 @swagger_auto_schema(method="post", tags=["wallet"], request_body=TransferSerializer)
 @permission_classes((IsAuthenticated,))
 @api_view(['POST'])
@@ -73,33 +74,10 @@ def transfer_to_wallet(request):
 
     if request.method == 'POST':
         serializers = TransferSerializer(data=request.data)
+
         if serializers.is_valid():
             data = serializers.data
-
-            payload = {
-                "id": "{{$randomUUID}}",
-                "method": "transfer.proceed",
-                "params": {
-                    "number": f"{data['number']}",
-                    "expire": f"{data['expire']}",
-                    "receiver": f"{wallet.card_number}",
-                    "amount": f"{data['amount']}",
-                }
-            }
-            try:
-                resp_data = requests.post(url=WALLET_URL, json=payload, headers={"token": f"{settings.WALLET_TOKEN}"})
-                if resp_data.json()['status']:
-                    return Response(resp_data.json())
-                else:
-                    token = login_to()
-                    try:
-                        resp_data = requests.post(url=WALLET_URL, json=payload, headers={"token": f"{token}"})
-                        TransferModel.objects.create(wallet=wallet, tr_id=resp_data.json()['result']['tr_id'])
-                    except:
-                        return Response({'message': 'Service is not working.'})
-                return Response(resp_data.json())
-            except:
-                return Response(status.HTTP_404_NOT_FOUND)
+            return transfer_service(wallet, data)
 
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -118,28 +96,13 @@ def withdraw_from_wallet(request):
         if serializers.is_valid():
             data = serializers.data
 
-            payload = {
-                "id": "{{$randomUUID}}",
-                "method": "transfer.proceed",
-                "params": {
-                    "number": f"{wallet.card_number}",
-                    "expire": f"{wallet.expire}",
-                    "receiver": f"{data['number']}",
-                    "amount": f"{data['amount']}",
-                }
-            }
-            try:
-                resp_data = requests.post(url=WALLET_URL, json=payload, headers={"token": f"{settings.WALLET_TOKEN}"})
-                TransferModel.objects.create(wallet=wallet, tr_id=resp_data.json()['result']['tr_id'])
-            except:
-                return Response(status.HTTP_404_NOT_FOUND)
-
-            return Response(resp_data.json())
+            resp_data = withdraw_from_wallet_service(wallet, data)
+            return resp_data
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @swagger_auto_schema(method="post", tags=["wallet"], request_body=WalletHistorySerializer)
-@permission_classes((IsAuthenticated,))
+@permission_classes([IsAuthenticated])
 @api_view(['POST'])
 def history_wallet(request):
     try:
